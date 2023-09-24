27403a547f8ef8017290f0d9121d2b7c92fe7d36
Cristian Carreño León <icristian.mac@gmail.com>
2023-09-24 12:52:01 -0300

    add www

---
+++ www/4485.c0c6f7de196ba77f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4485],{4485:(u,n,e)=>{e.r(n),e.d(n,{SplashScreenWeb:()=>_});var s=e(5861),r=e(2726);class _ extends r.Uw{show(t){return(0,s.Z)(function*(){})()}hide(t){return(0,s.Z)(function*(){})()}}}}]);
